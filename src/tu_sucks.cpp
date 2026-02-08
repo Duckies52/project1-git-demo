c68da9f0215ca7a19563f7fc46567c3c958350e1
ushara <ushara0513@gmail.com>
2026-02-08 06:52:27 -0600

    Updated tu_sucks.cpp locally befor pushing

--- src/tu_sucks.cpp
+++ src/tu_sucks.cpp
@@ -1,1 +1,15 @@
-
+/*
+Hullabaloo Caneck Caneck
+Hullabaloo, Caneck! Caneck!
+Good-bye to texas university
+So long to the orange and the white
+Good luck to dear old Texas Aggies
+They are the boys that show the real old fight
+The eyes of Texas are upon you
+That is the song they sing so well (Sounds like hell!)
+So good-bye to texas university
+Were going to beat you all to
+Chig-gar-roo-gar-rem
+Chig-gar-roo-gar-rem
+Rough! Tough! Real Stuff! Texas A&M
+*/
